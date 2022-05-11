7bc25e7554a2b3c450b35207c78c811f56a6cf24
udofritzke <udo.fritzke@gmail.com>
2022-05-11 10:55:05 -0300

    Versão com 2 fragmentos: framento 1 atualiza texto de fragmento 2 clicando-se em botões e textos do fragmento 1

--- app/src/main/java/com/example/exemplofragmento/Fragmento1.java
+++ app/src/main/java/com/example/exemplofragmento/Fragmento1.java
@@ -1,13 +1,23 @@
 package com.example.exemplofragmento;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class Fragmento1 extends Fragment {
+    private Button Botao1;
+    private Button Botao2;
+    private TextView texto1;
+    private View v;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +27,41 @@ public class Fragmento1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmento1, container, false);
+        v = inflater.inflate(R.layout.fragmento1, container, false);
+
+        Botao1 = (Button) v.findViewById(R.id.button1);
+        Botao1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("prints", "botao 1");
+                View outro_fragmento = inflater.inflate(R.layout.fragmento2, container, false);
+                TextView txt = (TextView) Fragmento2.frgto2.findViewById(R.id.texto_frg2);
+                if (txt != null) txt.setText("Apertou botão 1");
+            }
+        });
+        Botao2 = (Button) v.findViewById(R.id.button2);
+        Botao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("prints", "botao 2");
+                View outro_fragmento = inflater.inflate(R.layout.fragmento2, container, false);
+                TextView txt = (TextView) Fragmento2.frgto2.findViewById(R.id.texto_frg2);
+                if (txt != null) txt.setText("Apertou botão 2");
+            }
+        });
+
+        texto1 = (TextView) v.findViewById(R.id.texto1);
+        texto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("prints", "texto 1");
+                View outro_fragmento = inflater.inflate(R.layout.fragmento2, container, false);
+                TextView txt = (TextView) Fragmento2.frgto2.findViewById(R.id.texto_frg2);
+                if (txt != null) txt.setText("Apertou texto 1");
+            }
+        });
+
+
+        return v;
     }
 }
--- app/src/main/java/com/example/exemplofragmento/Fragmento2.java
+++ app/src/main/java/com/example/exemplofragmento/Fragmento2.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
 
 public class Fragmento2 extends Fragment {
-
+    static  View frgto2;
+    ViewGroup c;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragmento2, container, false);
+        frgto2 = inflater.inflate(R.layout.fragmento2, container, false);
+        c = container;
+        return frgto2; //inflater.inflate(R.layout.fragmento2, container, false);
     }
-
 }
